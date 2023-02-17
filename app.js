6afb2d87820be67750ef7922f5d51c0f31f111fc
Perriesha <pmbyrd@gmail.com>
2023-02-16 20:24:57 -0800

    Can do single operations

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const $round = $(".math-round");
 let $result = $(".result");
 let displayValue = "";
 
-
-
 // *event listeners
 $clear.on("click", function () {
   console.debug("clear button clicked");
@@ -62,14 +60,9 @@ function getMemory() {
   displayValue += $(this).val();
   $displayResult.text(displayValue);
   console.log("clicked", $(this).val());
-}  
+}
 
 function calculate(displayValue) {
-    // the operators are in an object must handle the extraction of the key
-   let start = 0
-   let total = 0
-//    let result = null
-
   const operatorsKeysAndValues = {
     "+": add,
     "-": subtract,
@@ -80,132 +73,39 @@ function calculate(displayValue) {
     "x^y": exponent,
   };
   console.debug("calculate() called");
-//   need to set a recurssive function so that way calculate is continuosly called as long as there is an operator stored
   const operators = Object.keys(operatorsKeysAndValues);
-    console.log(operators);
-    // get the display value
-    displayValue = $displayResult.text();
-    console.log(displayValue);
-    // loop through the operators
-    // function sum(n) {
-    //     if (n <= 1) {
-    //       return n;
-    //     }
-    //     return n + sum(n - 1);
-    //   }
-    while (operators.length > 0) {
-        for (let i = 0; i < operators.length; i++) {
-            // check if the display value includes the operator
-            if (displayValue.includes(operators[i])) {
-                console.log(operators[i]);
-                // split the display value at the index after the operator into a seperate array
-                let [a, b] = displayValue.split(operators[i]);
-                a = Number(a);
-                b = Number(b);
-                let result = operatorsKeysAndValues[operators[i]](a, b);
-                console.log(`${a} ${operators[i]} ${b} = ${result}`);
-                $result.text(result);
-                return operatorsKeysAndValues[operators[i]](a, b);
-            }
-            return result += operatorsKeysAndValues[operators[i]](a, b);
-        }
-    }
-    // for (let i = 0; i < operators.length; i++) {
-    //     // check if the display value includes the operator
-    //     if (displayValue.includes(operators[i])) {
-    //         console.log(operators[i]);
-    //         // split the display value at the index after the operator into a seperate array
-    //         let [a, b] = displayValue.split(operators[i]);
-    //         a = Number(a);
-    //         b = Number(b);
-    //         let result = operatorsKeysAndValues[operators[i]](a, b);
-    //         console.log(`${a} ${operators[i]} ${b} = ${result}`);
-    //         $result.text(result);
-    //         return operatorsKeysAndValues[operators[i]](a, b);
-    //     }
-    //     return result += operatorsKeysAndValues[operators[i]](a, b);
-    // }
+  console.log(operators);
+  displayValue = $displayResult.text();
+  console.log(displayValue);
+  // loop through the operators
+  // function sum(n) {
+  //     if (n <= 1) {
+  //       return n;
+  //     }
+  //     return n + sum(n - 1);
+  //   }
+  let displayValueArray = displayValue.split("");
+  displayValueArray.map(function(operators, index) {
+      console.table(operators, index);
+  })
+  for (let i = 0; i < operators.length; i++) {
+    if (displayValue.includes(operators[i])) {
+      let [a, b] = displayValue.split(operators[i]);
+      a = Number(a);
+      b = Number(b);
+      let restOfValues = displayValue.split(operators[i]);
+      
+    //   I need to store the rest of the values to access them again later
+    // I am currently only storing three values at a time but if there are more than three they disappear
+        console.log(restOfValues);
+      let result = operatorsKeysAndValues[operators[i]](a, b);
+      console.log(`${a} ${operators[i]} ${b} = ${result}`);
+      $result.text(result);
+      console.log(displayValue);
+      return operatorsKeysAndValues[operators[i]](a, b);
+    } 
+  }
 }
-
-  // try using the already included text to practice
-  // $displayValue = $displayResult.text();
-  // if($displayValue.includes("+")) {
-  //     let [a, b] = $displayValue.split("+");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = add(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("-")) {
-  //     let [a, b] = $displayValue.split("-");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = subtract(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("*")) {
-  //     let [a, b] = $displayValue.split("*");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = multiply(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("÷")) {
-  //     let [a, b] = $displayValue.split("÷");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = divide(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("^")) {
-  //     let [a, b] = $displayValue.split("^");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = exponent(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("%")) {
-  //     let [a] = $displayValue.split("%");
-  //     a = Number(a);
-  //     op = percent(a);
-  //     console.log(a, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("±")) {
-  //     let [a] = $displayValue.split("±");
-  //     a = Number(a);
-  //     op = negate(a);
-  //     console.log(a, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("√")) {
-  //     let [a] = $displayValue.split("√");
-  //     a = Number(a);
-  //     op = squareRoot(a);
-  //     console.log(a, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("π")) {
-  //     let [a] = $displayValue.split("π");
-  //     a = Number(a);
-  //     op = circumfrance(a);
-  //     console.log(a, op);
-  //     $result.text(op);
-  // }
-  // if($displayValue.includes("x^y")) {
-  //     let [a, b] = $displayValue.split("x^y");
-  //     a = Number(a);
-  //     b = Number(b);
-  //     op = exponent(a, b);
-  //     console.log(a, b, op);
-  //     $result.text(op);
-  // }
-
 
 // *math functions
 
