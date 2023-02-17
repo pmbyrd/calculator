7c85bcd2cb0daf8ae2f8b5c6bc9d2755eac09d41
Perriesha <pmbyrd@gmail.com>
2023-02-16 16:54:26 -0800

    Addition working

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-console.log('Hello World!');
-// intialize varaibles
-// *elements from the html
-const $displayResult = $(".result");
+console.log("Hello World!");
+
+const $displayResult = $(".input-result");
 const $clear = $(".clear");
 const $numbers = $(".number");
 const $form = $("form");
@@ -9,26 +8,24 @@ const $operators = $(".math-operator");
 const $memory = $(".memory");
 const $func = $(".math-func");
 const $round = $(".math-round");
-
-
-
-// *elements to added by me
+let $result = $(".result");
 let $displayValue = "";
-
+$displayValue.length = 40;
+console.log($displayValue);
 
 // *event listeners
 $clear.on("click", function () {
-    console.debug("clear button clicked");
-    $displayValue = "";
-    $displayResult.text($displayValue);
+  console.debug("clear button clicked");
+
+  $displayValue = "";
+  $displayResult.text($displayValue);
 });
 
 $form.on("submit", function (e) {
-    console.debug("form submitted");
-    e.preventDefault();
-    if(($(this).hasClass("number"))){
-        console.log("number clicked");
-    }
+  console.debug("form submitted");
+  e.preventDefault();
+  calculate();
+  console.log("clicked", "form submitted");
 });
 
 $numbers.on("click", getNumbers);
@@ -36,19 +33,80 @@ $operators.on("click", getOperator);
 
 // *functions
 function getNumbers() {
-    console.debug("getNumbers() called");
+  console.debug("getNumbers() called");
   $displayValue += $(this).val();
   $displayResult.text($displayValue);
   console.log("clicked", $(this).val());
 }
 
 function getOperator() {
-    console.debug("getOperator() called");
-    $displayValue += $(this).val();
-    $displayResult.text($displayValue);
+  console.debug("getOperator() called");
+  $displayValue += $(this).val();
+  $displayResult.text($displayValue);
   console.log("clicked", $(this).val());
 }
 
+function getFunc() {
+  console.debug("getFunc() called");
+  $displayValue += $(this).val();
+  $displayResult.text($displayValue);
+  console.log("clicked", $(this).val());
+}
+
+function getRound() {
+  console.debug("getRound() called");
+  $displayValue += $(this).val();
+  $displayResult.text($displayValue);
+  console.log("clicked", $(this).val());
+}
+
+function getMemory() {
+  console.debug("getMemory() called");
+  $displayValue += $(this).val();
+  $displayResult.text($displayValue);
+  console.log("clicked", $(this).val());
+}
+
+function calculate(a,b, op) {
+    console.debug("calculate() called");
+    // try using the already included text to practice
+    $displayValue = $displayResult.text();
+    if($displayValue.includes("+")) {
+        let [a, b] = $displayValue.split("+");
+        a = Number(a);
+        b = Number(b);
+        op = add(a, b);
+        console.log(a, b, op);
+        $result.text(op);   
+    }
+ 
+    
+}
 
 
+// *math functions
 
+function add(a, b = 0) {
+  return a + b;
+}
+function subtract(a, b = 0) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function exponent(a, b) {
+  return a ** b;
+}
+function percent(a) {
+  return a / 100;
+}
+function negate(a) {
+  return -a;
+}
+function squareRoot(a) {
+  return Math.sqrt(a);
+}
