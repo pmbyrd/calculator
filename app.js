168f437a4c5a580887ec2c21ad2104247a579fde
pmbyrd <pmbyrd@gmail.com>
2025-06-21 20:56:37 -0700

    feat: math func that take a single variable are working

--- app.js
+++ app.js
@@ -11,27 +11,30 @@ const operators = {
   "-": (a, b) => a - b,
   "*": (a, b) => a * b,
   "÷": (a, b) => a / b,
-  "**": (a, b) => a ** b,
+  "x^y": (a, b) => a ** b,
 };
 
 const mathFunctions = {
   "%": (a) => a / 100,
   π: (a) => 2 * Math.PI * a,
-  //TODO add in negate
-  "+-": (a) => -a,
+  "√x": (a) => Math.sqrt(a),
 };
+
+const negate = "±";
 
 const clear = document.querySelector(".clear");
 const input = document.querySelector(".input-result");
 const pad = document.querySelector(".pad");
 const evaluateBtn = document.querySelector(".evaluate");
 const result = document.querySelector(".result");
+const mathFunctionBtns = document.querySelectorAll(".math-func");
 let mathOperator = 0;
 
 function handleInput(e) {
   let value = e.target.value;
   if (e.target.value === "CE") return;
   if (e.target.value === "=") return;
+  if (e.target.classList.contains("math-func")) return;
   if (e.target.classList.contains("math-operator")) {
     if (input.textContent.length === 0) return;
     mathOperator++;
@@ -44,6 +47,27 @@ function handleInput(e) {
 function getOperatorKeys() {
   const keys = Object.keys(operators).join(" ");
   return keys;
+}
+
+function getMathFunctionsKeys() {
+  const keys = Object.keys(mathFunctions).join(" ");
+  return keys;
+}
+
+function handleMathFunction(e) {
+  let sum;
+  let mathFunc;
+  if (mathOperator > 0) return;
+  if (input.textContent === 0) return;
+  a = input.textContent;
+  a = +a;
+  if (e.target.classList.contains("math-func")) {
+    mathFunc = mathFunctions[e.target.value];
+    sum = mathFunc(a);
+    sum = roundDecimal(sum)
+    result.textContent = sum;
+    input.textContent = "";
+  }
 }
 
 function getOperator() {
@@ -72,7 +96,7 @@ function getVariables() {
     console.log(true, operator);
   }
   let [a, b] = content.split(operator);
-  console.log(+a, +b)
+  console.log(+a, +b);
   return [+a, +b, operator];
 }
 
@@ -81,20 +105,19 @@ function operate(a, b, operator) {
   [a, b, operator] = getVariables();
   let mathFunc = operators[operator];
   sum = mathFunc(a, b);
-  return roundDecimal(sum)
+  return roundDecimal(sum);
 }
 
-function roundDecimal(sum){
+function roundDecimal(sum) {
   if (sum % 1 !== 0) {
-    sum = Math.round(sum * 100) / 100;
+    sum = Math.round(sum * 10000) / 10000;
   }
   return sum;
 }
 
-function updateResult() {
+function updateResult(sum) {
   //when evaluateBtn is pressed I want to change show the results of the operate to the result and to clear the input
-  let sum = operate();
-  console.log(sum);
+  sum = operate();
   result.textContent = sum;
   input.textContent = "";
   mathOperator = 0;
@@ -108,3 +131,6 @@ clear.addEventListener("click", () => {
   input.textContent = "";
 });
 evaluateBtn.addEventListener("click", updateResult);
+mathFunctionBtns.forEach((btn) => {
+  addEventListener("click", handleMathFunction);
+});
